d2922b6627a73205f47801712cb5f997f12dd89a
James Martineau <jamesmartineau@Jamess-MacBook-Air.local>
2018-10-13 19:22:57 -0400

    initial config to hook up node to postgres started

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = process.env.PORT || 5000;
+
+// Log requests to the console.
+app.use(logger('dev'));
+
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // API calls
 app.get('/api/hello', (req, res) => {
