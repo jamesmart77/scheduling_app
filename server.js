a2d636d3d41cd9876377d8bc0e93e41ad5603f81
James Martineau <jamesmartineau@Jamess-MacBook-Air.local>
2019-01-23 14:56:00 -0500

    updated server.js to sync sequelize

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const db = require('./ORM/models');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -22,8 +23,22 @@ if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
   // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
-}
-app.listen(port, () => console.log(`Listening on port ${port}`));
+  
+  db.sequelize.sync().then(() => {
+    app.listen(port, () => {
+        console.log("App listening on PORT " + port);
+    });
+  });
+
+} else {
+  db.sequelize.sync({
+      force: true
+  }).then(() => {
+      app.listen(port, () => {
+          console.log("App listening on PORT " + port);
+      });
+  });
+};
